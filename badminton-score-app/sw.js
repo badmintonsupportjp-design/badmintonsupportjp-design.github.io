33ca35799fbd1b988f06149365389e07684ad1aa
badmintonsupportjp-design <badminton.support.jp@gmail.com>
2026-06-27 18:10:22 +0900

    Add files via upload

--- badminton-score-app/sw.js
+++ badminton-score-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bscore-cache-v2';
+﻿const CACHE_NAME = 'bscore-cache-v3';
 const urlsToCache = [
   './',
   './index.html',
@@ -51,3 +51,4 @@ self.addEventListener('activate', event => {
     })
   );
 });
+
